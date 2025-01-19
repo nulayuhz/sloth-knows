97cb365128926a07d0b6563e5f15d3f321d9c626
yalunzhu <yalunzhu@gmail.com>
2025-01-18 23:59:52 -0500

    add DB, and user creation api

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { hash } from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email },
+    });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this email already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username },
+    });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this username already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const newUser = await db.user.create({
+      data: { email, username, password: hashedPassword },
+    });
+    return NextResponse.json(
+      { user: newUser, message: "User created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {}
+}
