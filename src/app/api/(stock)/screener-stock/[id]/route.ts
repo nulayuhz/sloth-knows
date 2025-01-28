68a8e2f794f165872a4cf0bc711e07a40275ebbe
yalunzhu <yalunzhu@gmail.com>
2025-01-28 00:10:13 -0500

    add update screenerstock api

---
+++ src/app/api/(stock)/screener-stock/[id]/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  const { id } = await params;
+
+  try {
+    const existingScreener = await db.screenerStock.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+    console.log(body);
+    if (!existingScreener) {
+      return NextResponse.json({ data: "stock not found" }, { status: 400 });
+    }
+
+    const updateUser = await db.screenerStock.update({
+      where: {
+        id: Number(id),
+      },
+      data: body,
+    });
+    const updateUserWithBigInt = JSON.stringify(updateUser, (_, v) =>
+      typeof v === "bigint" ? v.toString() : v
+    );
+
+    return NextResponse.json(updateUserWithBigInt, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
